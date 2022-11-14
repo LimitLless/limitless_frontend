6580cc676631808d45feea03655de016a12361cc
urmatbekova <urmatbekovanurkyz003@gmail.com>
2022-11-14 22:35:39 +0600

    fixed conflict

--- components/pages/user/TabContent.tsx
+++ components/pages/user/TabContent.tsx
@@ -50,13 +50,6 @@ const useContactsStyles = makeStyles((theme: Theme) => ({
             color: theme.palette.secondary.main,
         }
     },
-<<<<<<< HEAD
-        color: theme.palette.secondary.main,
-
-    },
-
-=======
->>>>>>> fd7e6ac667b96d237719ab13c3b4c8430ca1c30a
 }));
 
 
